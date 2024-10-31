1789e15bd19acab36fb6e7c87614aa4f39483113
Anh Dung Phan <Aphan5160@conestogac.on.ca>
2024-10-31 13:12:43 -0400

    [CSCN71020_A3] Add: comments for function

--- CSCN71020_A3/Main.c
+++ CSCN71020_A3/Main.c
@@ -12,16 +12,20 @@ char* playRockPaperScissorGame(char player1[], char player2[])
 	if (strcmp(player2, "Rock") != 0 && strcmp(player2, "Paper") != 0 && strcmp(player2, "Scissors") != 0)
 		return "Invalid";
 
+	// Return Draw if two players have the same inputs
 	if (strcmp(player1, player2) == 0) {
 		return "Draw";
 	}
 
+	// If Player1 shapes Rock, then Player2 shapes Paper => Player2 wins, otherwise Player1 wins
 	if (strcmp(player1, "Rock") == 0) {
 		return strcmp(player2, "Paper") == 0 ? "Player2" : "Player1";
 	}
+	// If Player1 shapes Paper, then Player2 shapes Scissors => Player2 wins, otherwise Player1 wins
 	if (strcmp(player1, "Paper") == 0) {
 		return strcmp(player2, "Scissors") == 0 ? "Player2" : "Player1";
 	}
+	// If Player1 shapes Scissors, then Player2 shapes Rock => Player2 wins, otherwise Player1 wins
 	if (strcmp(player1, "Scissors") == 0) {
 		return strcmp(player2, "Rock") == 0 ? "Player2" : "Player1";
 	}
