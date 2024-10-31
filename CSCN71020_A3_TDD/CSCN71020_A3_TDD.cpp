dbb541ee941c224068cbea797e859cd7d4d88703
Anh Dung Phan <Aphan5160@conestogac.on.ca>
2024-10-31 12:36:48 -0400

    [CSCN71020_A3] Create: first unit test

--- CSCN71020_A3_TDD/CSCN71020_A3_TDD.cpp
+++ CSCN71020_A3_TDD/CSCN71020_A3_TDD.cpp
@@ -1,4 +1,5 @@
 #include "pch.h"
+#include "string.h"
 #include "CppUnitTest.h"
 
 // Tell compiler this is external and written in C
@@ -15,6 +16,19 @@ namespace CSCN71020A3TDD
 		
 		TEST_METHOD(TestMethod1)
 		{
+			// Declare variables to store two player inputs
+			char player1[] = "Rock";
+			char player2[] = "Paper";
+			// Declare variables to store the actual and expected results
+			char expected[] = "Player2";
+			char* actual;
+
+			// Call playRockPaperScissorGame function and store the result to 'actual' var
+			actual = playRockPaperScissorGame(player1, player2);
+
+			// Check if actual result of the function equal to the expected result
+			bool isActualEqualExpected = strcmp(actual, expected) == 0;
+			Assert::IsTrue(isActualEqualExpected);
 		}
 	};
 }
