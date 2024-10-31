8c3771836694098af20ad15d57cfe352bd4f1724
Anh Dung Phan <Aphan5160@conestogac.on.ca>
2024-10-31 11:59:34 -0400

    Add project files.

---
+++ CSCN71020_A3/Main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+char* playRockPaperScissorGame(char player1[], char player2[]);
+
+char* playRockPaperScissorGame(char player1[], char player2[])
+{
+	return NULL;
+}
+
+void main()
+{
+	return;
+}
---
+++ CSCN71020_A3_TDD/CSCN71020_A3_TDD.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+// Tell compiler this is external and written in C
+extern "C" char* playRockPaperScissorGame(char player1[], char player2[]);
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CSCN71020A3TDD
+{
+	TEST_CLASS(CSCN71020A3TDD)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
